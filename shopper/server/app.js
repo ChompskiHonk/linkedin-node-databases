64589f945447b1f93079bc10d21eb136fdc050f7
ChompskiHonk <77406368+ChompskiHonk@users.noreply.github.com>
2021-05-09 12:38:08 -0400

    Update app.js

--- shopper/server/app.js
+++ shopper/server/app.js
@@ -9,15 +9,11 @@ const BasketService = require("./services/BasketService");
 
 module.exports = (config) => {
   const app = express();
-
-  // view engine setup
   app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "pug");
-
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-
-  app.set("trust proxy", 1); // trust first proxy
+  app.set("trust proxy", 1);
   app.use(
     session({
       store: new RedisStore({ client: config.redis.client }),
@@ -31,16 +27,15 @@ module.exports = (config) => {
   app.get("/favicon.ico", (req, res) => {
     res.status(204);
   });
+  
   app.get("/robots.txt", (req, res) => {
     res.status(204);
   });
 
-  // Define 'global' template variables here
   app.use(async (req, res, next) => {
     // To show the application name on the page
     res.locals.applicationName = config.applicationName;
 
-    // Set up flash messaging
     if (!req.session.messages) {
       req.session.messages = [];
     }
@@ -71,22 +66,19 @@ module.exports = (config) => {
 
   app.use("/", routeHandler(config));
 
-  // catch 404 and forward to error handler
+  
   app.use((req, res, next) => {
     const err = new Error(`Not Found (${req.url})`);
     err.status = 404;
     next(err);
   });
-
-  // error handler
+  
   app.use((err, req, res) => {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
-    res.render("error");
+    res.render("Error");
   });
 
   return app;
